d0244e9c8135529124f80f0e5e2df195bff08e35
zubair bin ferdous <zubairbinf@gmail.com>
2021-03-29 22:59:34 +0600

    first commit

---
+++ condition.js
@@ -0,0 +1,43 @@
+price = 12
+
+if ( price < 10 ) {
+    console.log('i will make this1');
+}
+
+if ( price > 10 ) {
+    console.log('i will make this2');
+}
+
+if ( price == 12 ) {
+    console.log('i will make this3');
+}
+
+if ( price != 10 ) {
+    console.log('i will make this4');
+}
+
+if ( price != 11 ) {
+    console.log('i will make this5');
+}
+else{
+    console.log( ' no i can not');
+}
+
+var jobFound = false;
+var saveAmount = 500000;
+
+if ( jobFound == true  && saveAmount > 300000 )  {
+    console.log('ok we going to found girl');
+}
+
+// if ( jobFound == true  || saveAmount > 300000 )  {
+//     console.log('ok we going to found girl');
+// }
+
+else if ( jobFound = true ) {
+    console.log('ok are you ready to go');
+}
+
+else{
+    console.log('no , not now');
+}
---
+++ hello.js
@@ -0,0 +1,93 @@
+console.log(90);
+
+var bananaPrice = 12;
+console.log(bananaPrice);
+
+var carPrice = 2000;
+var rentPrice = 1500;
+var busPrice = 5000;
+
+console.log(carPrice , rentPrice , busPrice);
+console.log(typeof carPrice);
+
+var Name = 'mrs bin jon';
+console.log(typeof Name);
+
+var Hot = true;
+var Rich = false;
+
+console.log(typeof Hot);
+
+var Name = 'Hero Bin';
+
+var promise = 'i will work HERD to become a programmer';
+var Name = `name to be developer`;
+console.log (promise.toUpperCase()) ;
+console.log (promise.indexOf('will'));
+console.log(promise.split(' '));
+
+var Num = 0.1;
+var Mun = 0.2;
+var total = Mun + Num;
+
+result =  total.toFixed(2);
+console.log(result);
+
+number = ''+Mun;
+console.log(Num + number);
+
+number = +Mun;
+console.log(Num + number);
+
+number = parseFloat(Mun);
+console.log(Num + number);
+
+number = parseInt(Mun);
+console.log(Num + number);
+
+
+var Fname = 'ninja';
+var lname = 'fez';
+
+var result = lname + " " + Fname;
+console.log(result);
+
+
+var price1 = 10;
+var price2 = 200;
+price2++;
+price2--;
+console.log(price2);
+
+var total = price1 + price2;
+console.log(total);
+
+var total = price1 - price2;
+console.log(total);
+
+var total = price1 * price2;
+console.log(total);
+
+var total = price1 / price2;
+console.log(total.toFixed(5));
+
+var total = price1 % price2;
+console.log(total);
+
+var number = 5.000;
+var number = 5.9;
+var result = Math.abs(number);
+var result = Math.round(number);
+var result = Math.ceil(number);
+var result = Math.floor(number);
+console.log(result);
+
+
+var random = Math.random()*1000;
+var result = Math.round(random);
+console.log(result);
+
+var date = new Date('1971/12/15');
+console.log(date);
+
+
